1f0849a3acb8cff79281a80ebb03bd048cc0c0ec
SR-97 <sal.rh@hotmail.com>
2024-03-21 16:07:01 -0700

    s

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
-/**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
- */
-void main(void)
-{
-	print("test");
+void generate_key(char *username) {
+    int len = strlen(username);
+    int i;
+    char key[7];
+
+    for (i = 0; i < len; i++)
+        key[i] = username[i] ^ len;
+    
+    for (; i < 7; i++)
+        key[i] = '\0';
+
+    printf("%s\n", key);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s username\n", argv[0]);
+        return 1;
+    }
+
+    generate_key(argv[1]);
+    return 0;
 }
